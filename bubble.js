80b7d4143a29b7fa1fa5c3ae8e777b3f331f77fa
Steven Dowd <swdowd@gmail.com>
2023-11-10 14:27:33 -0500

    finished bubble and merge sorts

--- bubble.js
+++ bubble.js
@@ -1,3 +1,20 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--){
+    let hasSwapped = false;
+    for(let j = 0; j < i; j++){
+      if (arr[j] > arr[j + 1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        hasSwapped = true;
+      }
+    }
+
+  if (!hasSwapped){
+    return arr;
+  }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,39 @@
-function merge() {}
+function merge(arrA, arrB) {
+  let pointerA = 0;
+  let pointerB = 0;
+  let newArr = [];
 
-function mergeSort() {}
+  while((pointerA < arrA.length) && (pointerB < arrB.length)){
+    if (arrA[pointerA] <= arrB[pointerB]){
+      newArr.push(arrA[pointerA]);
+      pointerA++;
+    } else {
+      newArr.push(arrB[pointerB]);
+      pointerB++;
+    }
+  }
+
+  if (pointerA < arrA.length){
+    newArr = newArr.concat(arrA.slice(pointerA));
+  }
+
+  if (pointerB < arrB.length){
+    newArr = newArr.concat(arrB.slice(pointerB));
+  }
+
+  return newArr;
+
+}
+
+function mergeSort(arr) {
+  if (arr.length < 2){
+    return arr;
+  }
+  let midpoint = Math.floor(arr.length / 2);
+
+  let left = mergeSort(arr.slice(0, midpoint))
+  let right = mergeSort(arr.slice(midpoint));
+  return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
